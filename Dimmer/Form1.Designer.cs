920b58fd62611085a61871cc5184782ede875f46
GittyGetty <riese8@gmail.com>
2015-01-18 12:54:35 -0800

    A System Tray Tool to Control Screen Brightness

---
+++ Dimmer/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Dimmer
+{
+    class MyTrackBar : System.Windows.Forms.TrackBar 
+    {
+        [DllImport("user32.dll")]
+        public extern static int SendMessage(IntPtr hWnd, uint msg, int wParam, int lParam);
+
+        private static int MakeParam(int loWord, int hiWord)
+        {
+            return (hiWord << 16) | (loWord & 0xffff);
+        }
+
+        protected override void OnGotFocus(EventArgs e)
+        {
+            base.OnGotFocus(e);
+            SendMessage(this.Handle, 0x0128, MakeParam(1, 0x1), 0);
+        }
+    }
+
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        bool myVisible = false;
+        protected override void SetVisibleCore(bool value)
+        {
+            base.SetVisibleCore(myVisible);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.trackBar1 = new Dimmer.MyTrackBar();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar1.LargeChange = 100;
+            this.trackBar1.Location = new System.Drawing.Point(12, 12);
+            this.trackBar1.Maximum = 255;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(345, 27);
+            this.trackBar1.SmallChange = 10;
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.TickFrequency = 5;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Display Dimmer";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDown);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(93, 26);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 50);
+            this.ControlBox = false;
+            this.Controls.Add(this.trackBar1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.TopMost = true;
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private MyTrackBar trackBar1;
+    }
+}
+
---
+++ Dimmer/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dimmer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.trackBar1.Value = Program.GetGamma();
+            this.trackBar1.MouseWheel += trackBar1_MouseWheel;
+            this.MouseWheel += Form1_MouseWheel;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            Program.SetGamma(this.trackBar1.Value);
+        }
+
+
+        bool dragging = false;
+        int dx, dy;
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if ((Control.MouseButtons & MouseButtons.Left) == 0)
+            {
+                dragging = false;
+                return;
+            }
+
+            if (!dragging)
+            {
+                dragging = true;
+                dx = this.Location.X - Control.MousePosition.X;
+                dy = this.Location.Y - Control.MousePosition.Y;
+            }
+
+            int newX = Control.MousePosition.X + dx;
+            int newY = Control.MousePosition.Y + dy;
+            this.Location = new Point(newX, newY);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+            Application.Exit();
+        }
+
+        private void notifyIcon1_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.myVisible = !myVisible;
+            this.Visible = myVisible;
+            this.Focus();
+        }
+
+        void trackBar1_MouseWheel(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            this.Opacity *= e.Delta > 0 ? 1.1 : 0.9;
+            this.Opacity = Math.Max(0.1, this.Opacity);
+            var ee = (System.Windows.Forms.HandledMouseEventArgs)e;
+            ee.Handled = true;
+        }
+
+        void Form1_MouseWheel(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            this.Opacity *= e.Delta > 0 ? 1.1 : 0.9;
+            this.Opacity = Math.Max(0.1, this.Opacity);
+        }
+    }
+}
